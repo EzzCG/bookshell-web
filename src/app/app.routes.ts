f7548c576a7e620a2ce9c733e278d8f1df7d401c
naez@alumni.uv.es <72402860+officialEN10@users.noreply.github.com>
2026-02-16 15:27:25 +0100

    feature/standalone components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'books',
+    loadComponent: () =>
+      import('./features/books/pages/books-list/books-list').then((m) => m.BooksList),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/pages/login/login').then((m) => m.Login),
+  },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '**', redirectTo: 'books' },
+];
